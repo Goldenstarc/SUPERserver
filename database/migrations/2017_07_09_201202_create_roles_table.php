68b6c7c95230f304499eb6c5985391211eb47bd2
Amin <smartis2018@gmail.com>
2017-07-10 01:07:32 +0430

    admin control

---
+++ database/migrations/2017_07_09_201202_create_roles_table.php
@@ -0,0 +1,36 @@
+ <?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->boolean('canAddnewUser')->default(false);
+            $table->boolean('canDeleteUser')->default(false);
+            $table->boolean('canAddNewPost')->default(true);
+            $table->boolean('canEditPost')->default(false);
+            $table->boolean('canDeletePost')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('roles');
+    }
+}
