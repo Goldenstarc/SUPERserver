062d8a9859f3bbe30b9ef4cf3b4ba2b227edffcb
AminSaffar <aminakhavan75@gmail.com>
2017-07-10 14:54:29 +0430

    + create user form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,6 +4,13 @@
 
 @section('content')
     <h1>users Users</h1>
-
-
+    {!! Form::open(['method'=>'POST', 'action'=> 'AdminUserController@store']) !!}
+    <div class="form-group">
+        {!! Form::label('title', 'Title') !!}
+        {!! Form::text('title' , null ,['class'=>'form-control'])!!}
+    </div>
+    <div>
+        {!! Form::submit('create Post', ['class'=>'btn btn-primary'])!!}
+    </div>
+    {!! Form::close() !!}
 @stop
